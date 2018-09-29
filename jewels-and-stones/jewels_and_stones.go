4b4aaa53a97835190c421eaa8b05a04d0d173c2c
clinton <clintonwanason1@gmail.com>
2018-09-29 22:28:55 +0800

    solution for jewels and stones

--- jewels-and-stones/jewels_and_stones.go
+++ jewels-and-stones/jewels_and_stones.go
@@ -1,5 +1,13 @@
 package jewelsandstones
 
+import "strings"
+
 func jewelsAndStones(jewels string, stones string) int {
-	return 1
+	count := 0
+	for _, stone := range stones {
+		if strings.Contains(jewels, string(stone)) {
+			count++
+		}
+	}
+	return count
 }
