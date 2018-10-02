f87b6594b884ccb9b7a60306dd8ff5e6c220b1e8
clinton <clintonwanason1@gmail.com>
2018-10-02 23:22:13 +0800

    added solution for Unique Morse Code Words problem

--- unique-morse-code-words/unique_morse_representations.go
+++ unique-morse-code-words/unique_morse_representations.go
@@ -2,5 +2,46 @@ package uniquemorsecodewords
 
 // UniqueMorseRepresentations return the number of different transformations among all words we have.
 func UniqueMorseRepresentations(words []string) int {
+	morseCode := []string{".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."}
+	mappedMorse := mapMorseToLetter(morseCode)
+	transformations := []string{}
+	for _, char := range words {
+		transformations = append(transformations, convertToMorse(string(char), mappedMorse))
+	}
+	length := len(removeDuplicates(transformations))
+	return length
+}
 
+func removeDuplicates(words []string) []string {
+	keys := make(map[string]bool)
+	list := []string{}
+	for _, word := range words {
+		if _, value := keys[word]; !value {
+			keys[word] = true
+			list = append(list, string(word))
+		}
+	}
+	return list
+}
+
+func convertToMorse(word string, code map[string]string) string {
+	convertedStr := ""
+	for _, char := range word {
+		convertedStr += code[string(char)]
+	}
+	return convertedStr
+}
+
+func mapMorseToLetter(morse []string) map[string]string {
+	morseMap := make(map[string]string)
+	for i, code := range morse {
+		morseMap[string(toChar(i+1))] = code
+	}
+	return morseMap
+}
+
+func transformToMorse() {}
+
+func toChar(i int) rune {
+	return rune('a' - 1 + i)
 }
